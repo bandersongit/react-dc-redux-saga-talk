1fffaf33bda3110364ada5f197ac007a4c376459
banderson <bhafl2011@gmail.com>
2018-07-20 18:13:56 -0400

    fix tests

--- src/todos/sagas/createTodoSaga.test.ts
+++ src/todos/sagas/createTodoSaga.test.ts
@@ -1,8 +1,15 @@
 import { runSaga } from "redux-saga";
 import { AppActionType } from "@App/store/actions";
-import { createTodo, TodoActions } from "@App/todos/todoActions";
+import { createTodo, TodoActions, addTodo } from "@App/todos/todoActions";
 import { requestTodo, createTodoSaga } from "@App/todos/sagas/createTodoSaga";
 import { expectSaga } from 'redux-saga-test-plan';
+import { Todo } from "@App/todos/todoState";
+jest.mock('../serverApi/createServerTodo', () => {
+    let increment = 0;
+    return {
+        createServerTodo: (task: string) => new Promise<Todo>(res => res({task, isCompleted: false, id: `someId${increment++}`}))
+    };
+});
 
 describe("create todo saga", () => {
     test("should dispatch an add todo action with the task from the input create todo action", async () => {
@@ -19,13 +26,12 @@ describe("create todo saga", () => {
         expect(actions.some(a => a.type === TodoActions.ADD_TODO && a.payload.todo.task === task)).toBeTruthy();
     });
 
-    // todo fix test
-    test("should dispatch an add todo action with the task from a dispatched create todo action", async (done) => {
+    test("should dispatch an add todo action with the task from a dispatched create todo action", async () => {
         const task = "demonstrate redux-saga-test-plan";
         
         await expectSaga(createTodoSaga)
             .dispatch(createTodo({task}))
-            .put.like({ action: { type: TodoActions.ADD_TODO} })
+            .put.like({ action: addTodo({todo: {task, isCompleted: false} as Todo}) })
             .silentRun();
     });
 });
